c0af1d311d6c8e58e1decd23b7b8b3d5e9fcc0a3
Roxamir <101136913+Roxamir@users.noreply.github.com>
2024-02-12 18:17:40 -0800

    change dbconnection from MySQL to PostgreSQL, update resources

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ https://codedamn.com/news/python/defaulting-to-user-installatio-because-normal-s
 https://www.ams.usda.gov/grades-standards/tomato-sauce-grades-and-standards
 
 https://stackoverflow.com/questions/36708125/user-input-to-mysql-database
+
+https://www.freecodecamp.org/news/postgresql-in-python/
+
+https://www.tutorialspoint.com/postgresql/postgresql_create_table.htm
--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-import mysql.connector
+import psycopg2
  
 # connecting to the mysql server
-db = mysql.connector.connect(
+db = psycopg2.connect(
+    database="recipe_finder",
     host="localhost",
-    user="root",
-    passwd="Kawrcrc10717!!",
-    database="recipe_finder"
+    user="postgres",
+    password="cs361",
+    port="5432"
+    
 )
  
 # cursor object c
 c = db.cursor()
 
 # Ingredient Search SQL Query
-query = ("SELECT ingredientName, ingredientDescription FROM ingredients WHERE ingredientName LIKE %s")
+query = ("SELECT ingredient_name, ingredient_description FROM ingredients WHERE ingredient_name ILIKE %s")
 
 # user input for ingredient search
 ingredient = input("Search for an ingredient: ")
@@ -22,9 +24,10 @@ ingredient = input("Search for an ingredient: ")
 c.execute(query, ("%" + ingredient + "%",))
  
 # printing all the ingredients
-for i in c:
-    print(i)
-c = db.cursor()
+results = c.fetchall()
+
+for result in results:
+    print(result)
  
 # finally closing the database connection
 db.close()
