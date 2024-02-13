02322dc08df56f535c0c0b3d52f9a62d8ecef657
Roxamir <101136913+Roxamir@users.noreply.github.com>
2024-02-12 19:29:39 -0800

    removed old db_connector.py created new fucntions: recipe_search, ingredient_search added text UI and user input controls

--- main.py
+++ main.py
@@ -1,33 +1,93 @@
 import psycopg2
- 
-# connecting to the mysql server
-db = psycopg2.connect(
+import time
+
+def ingredient_search(ingredient):
+
+    # connecting to the postgres server
+    db = psycopg2.connect(
     database="recipe_finder",
     host="localhost",
     user="postgres",
     password="cs361",
     port="5432"
+    )
+
+    # cursor object c
+    c = db.cursor()
+
+    # Ingredient Search SQL Query
+    query = ("SELECT ingredient_name, ingredient_description FROM ingredients WHERE ingredient_name ILIKE %s")
     
-)
- 
-# cursor object c
-c = db.cursor()
+    # execute search query
+    c.execute(query, ("%" + ingredient + "%",))
 
-# Ingredient Search SQL Query
-query = ("SELECT ingredient_name, ingredient_description FROM ingredients WHERE ingredient_name ILIKE %s")
+    # searching animation
+    print("Searching")
+    for i in range(0,5):
+        time.sleep(1)
+        print("*")
 
-# user input for ingredient search
-ingredient = input("Search for an ingredient: ")
+    # storing results and printing matching ingredients
+    results = c.fetchall()
+    for result in results:
+        print(result)
+        print("\n")
+
+    # closing the database connection
+    db.close()
+
+def recipe_search(recipe):
+
+    # connecting to the postgres server
+    db = psycopg2.connect(
+    database="recipe_finder",
+    host="localhost",
+    user="postgres",
+    password="cs361",
+    port="5432"
+    )
+
+    # cursor object c
+    c = db.cursor()
+
+    # Recipe Search SQL Query
+    query = ("SELECT recipe_name, recipe_description FROM recipes WHERE recipe_name ILIKE %s")
+
+    # execute search query
+    c.execute(query, ("%" + recipe + "%",))
+
+    # searching animation
+    print("Searching")
+    for i in range(0,5):
+        time.sleep(1)
+        print("*")
+
+    # storing results and printing matching ingredients
+    results = c.fetchall()
+    for result in results:
+        print(result)
+        print("/n")
+
+    # closing the database connection
+    db.close()
 
 
+cmd = 9
 
-c.execute(query, ("%" + ingredient + "%",))
- 
-# printing all the ingredients
-results = c.fetchall()
+while cmd != 0:
+    print("""
+    Hello. Welcome to Recipe Finder v0.1. Pl an option:
+    
+    [1] Search for recipe
+    [2] Search for ingredient
+    [0] EXIT
+          
+      """)
+    cmd = int(input("Input a number then press 'ENTER': "))
 
-for result in results:
-    print(result)
- 
-# finally closing the database connection
-db.close()
+    if cmd == 1:
+        recipe = input("Please input recipe: ")
+        recipe_search(recipe)
+    elif cmd == 2:
+        ingredient = input("Please input ingredient: ")
+        ingredient_search(ingredient)
