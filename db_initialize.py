c8a1efa6e75d7b717e479f710b86819d8e3440e1
Roxamir <101136913+Roxamir@users.noreply.github.com>
2024-02-23 11:00:47 -0800

    Fix formatting of recipies table during initialization.

--- db_initialize.py
+++ db_initialize.py
@@ -47,15 +47,15 @@ def insert_data():
                     'Chocolate Chip Cookies',
                     'A chocolate chip cookie is a drop cookie that features chocolate chips or chocolate morsels as its distinguishing ingredient.',
                     'Preheat oven to 375 degrees F. Line three baking sheets with parchment paper and set aside.
-                    In a medium bowl mix flour, baking soda, baking powder and salt. Set aside.
-                    Cream together butter and sugars until combined.
-                    Beat in eggs and vanilla until light (about 1 minute).
-                    Mix in the dry ingredients until combined.
-                    Add chocolate chips and mix well.
-                    Roll 2-3 Tablespoons (depending on how large you like your cookies) of dough at a time into balls and place them evenly spaced on your prepared cookie sheets.
-                    Bake in preheated oven for approximately 8-10 minutes.
-                    Take them out when they are just barely starting to turn brown.
-                    Let them sit on the baking pan for 2 minutes before removing to cooling rack.',
+In a medium bowl mix flour, baking soda, baking powder and salt. Set aside.
+Cream together butter and sugars until combined.
+Beat in eggs and vanilla until light (about 1 minute).
+Mix in the dry ingredients until combined.
+Add chocolate chips and mix well.
+Roll 2-3 Tablespoons (depending on how large you like your cookies) of dough at a time into balls and place them evenly spaced on your prepared cookie sheets.
+Bake in preheated oven for approximately 8-10 minutes.
+Take them out when they are just barely starting to turn brown.
+Let them sit on the baking pan for 2 minutes before removing to cooling rack.',
                     'https://joyfoodsunshine.com/the-most-amazing-chocolate-chip-cookies/'
                     );
                     
@@ -64,11 +64,11 @@ def insert_data():
                     'Grilled Cheese',
                     'The grilled cheese (sometimes known as a toasted sandwich or cheese toastie) is a hot cheese sandwich typically prepared by heating slices of cheese between slices of bread with a cooking fat such as butter or mayonnaise on a frying pan, griddle, or sandwich toaster, until the bread browns and the cheese melts.',
                     'In a large bowl, beat cream cheese and mayonnaise until smooth.
-                    Stir in the cheeses, garlic powder and seasoned salt.
-                    Spread 5 slices of bread with the cheese mixture, about 1/3 cup on each.
-                    Top with remaining bread.
-                    Butter the outsides of sandwiches.
-                    In a skillet over medium heat, toast sandwiches for 4-5 minutes on each side or until bread is lightly browned and cheese is melted.',
+Stir in the cheeses, garlic powder and seasoned salt.
+Spread 5 slices of bread with the cheese mixture, about 1/3 cup on each.
+Top with remaining bread.
+Butter the outsides of sandwiches.
+In a skillet over medium heat, toast sandwiches for 4-5 minutes on each side or until bread is lightly browned and cheese is melted.',
                     'https://www.tasteofhome.com/recipes/the-ultimate-grilled-cheese/'
                     );
 
@@ -77,11 +77,11 @@ def insert_data():
                     'Spinach-Mushroom Scrambled Eggs',
                     'A simple and classic breakfast dish that incorporates eggs, spinach and mushrooms.',
                     'In a small bowl, whisk eggs, egg whites, salt and pepper until blended.
-                    In a small nonstick skillet, heat butter over medium-high heat.
-                    Add mushrooms; cook and stir 3-4 minutes or until tender.
-                    Add spinach; cook and stir until wilted. Reduce heat to medium.
-                    Add egg mixture; cook and stir just until eggs are thickened and no liquid egg remains.
-                    Stir in cheese.',
+In a small nonstick skillet, heat butter over medium-high heat.
+Add mushrooms; cook and stir 3-4 minutes or until tender.
+Add spinach; cook and stir until wilted. Reduce heat to medium.
+Add egg mixture; cook and stir just until eggs are thickened and no liquid egg remains.
+Stir in cheese.',
                     'https://www.tasteofhome.com/recipes/spinach-mushroom-scrambled-eggs/'
                     );
 
@@ -89,15 +89,15 @@ def insert_data():
                     VALUES(
                     'Roasted Tomato Soup',
                     'Tomato soup is a soup with tomatoes as the primary ingredient.
-                    It can be served hot or cold, and may be made in a variety of ways.
-                    It may be smooth in texture, and there are also recipes that include chunks of tomato, cream, chicken or vegetable stock, vermicelli, chunks of other vegetables and meatballs.
-                    Many companies have their own versions of tomato soup which all vary in taste, portions and ingredients.',
+It can be served hot or cold, and may be made in a variety of ways.
+It may be smooth in texture, and there are also recipes that include chunks of tomato, cream, chicken or vegetable stock, vermicelli, chunks of other vegetables and meatballs.
+Many companies have their own versions of tomato soup which all vary in taste, portions and ingredients.',
                     'Preheat oven to 400°.
-                    Place tomatoes, onion and garlic in a greased 15x10x1-in. baking pan.
-                    Drizzle with oil, sprinkle with thyme, salt and pepper and toss to coat.
-                    Roast until tender, 25-30 minutes, stirring once. Cool slightly.
-                    Working in batches, process tomato mixture and basil leaves in a blender until smooth.
-                    Transfer to a large saucepan; heat through. If desired, top with croutons and fresh basil.',
+Place tomatoes, onion and garlic in a greased 15x10x1-in. baking pan.
+Drizzle with oil, sprinkle with thyme, salt and pepper and toss to coat.
+Roast until tender, 25-30 minutes, stirring once. Cool slightly.
+Working in batches, process tomato mixture and basil leaves in a blender until smooth.
+Transfer to a large saucepan; heat through. If desired, top with croutons and fresh basil.',
                     'https://www.tasteofhome.com/recipes/roasted-tomato-soup-with-fresh-basil/'
                     );  
 
@@ -105,14 +105,14 @@ def insert_data():
                     VALUES(
                     'Garlic Bread',
                     'Garlic bread (also called garlic toast) consists of bread (usually a baguette, sour dough, or bread such as ciabatta), topped with garlic and occasionally olive oil or butter and may include additional herbs, such as oregano or chives.
-                    It is then either grilled until toasted or baked in a conventional or bread oven.',
+It is then either grilled until toasted or baked in a conventional or bread oven.',
                     'In a small bowl, mix the butter, cheese, basil, parsley and garlic until blended.
-                    Cut baguette crosswise in half; cut each piece lengthwise in half.
-                    Spread cut sides with butter mixture.
-                    Place on an ungreased baking sheet.
-                    Bake, uncovered, at 425° until lightly toasted, 7-9 minutes.
-                    Sprinkle with goat cheese; bake until goat cheese is softened, 1-2 minutes longer.
-                    Cut into smnaller slices.',
+Cut baguette crosswise in half; cut each piece lengthwise in half.
+Spread cut sides with butter mixture.
+Place on an ungreased baking sheet.
+Bake, uncovered, at 425° until lightly toasted, 7-9 minutes.
+Sprinkle with goat cheese; bake until goat cheese is softened, 1-2 minutes longer.
+Cut into smnaller slices.',
                     'https://www.tasteofhome.com/recipes/herb-happy-garlic-bread/'
                     );  
 
@@ -121,9 +121,9 @@ def insert_data():
                     'Spaghetti Sauce',
                     'A tomato sauce typically served with spaghetti. Usually contains herbs and meat.',
                     'In a Dutch oven, cook the beef, sausage, onions and garlic over medium heat until meat is no longer pink; drain.
-                    Transfer to a 5-qt. slow cooker.
-                    Stir in the tomatoes, tomato paste, water, sugar, Worcestershire sauce, oil and seasonings.
-                    Cook, covered, on low 8-10 hours. Discard bay leaves. Serve with spaghetti.',
+Transfer to a 5-qt. slow cooker.
+Stir in the tomatoes, tomato paste, water, sugar, Worcestershire sauce, oil and seasonings.
+Cook, covered, on low 8-10 hours. Discard bay leaves. Serve with spaghetti.',
                     'https://www.tasteofhome.com/recipes/stamp-of-approval-spaghetti-sauce/'
                     );  
 
@@ -131,7 +131,7 @@ def insert_data():
                     VALUES(
                     'Guacamole',
                     'Guacamole is an avocado-based dip, spread, or salad first developed in Mexico.
-                    In addition to its use in modern Mexican cuisine, it has become part of international cuisine as a dip, condiment and salad ingredient.',
+In addition to its use in modern Mexican cuisine, it has become part of international cuisine as a dip, condiment and salad ingredient.',
                     'Mash avocados with garlic and salt. Stir in remaining ingredients, adding tomatoes and mayonnaise if desired.',
                     'https://www.tasteofhome.com/recipes/homemade-guacamole/'
                     );  
@@ -140,23 +140,23 @@ def insert_data():
                     VALUES(
                     'Chicken Noodle Soup',
                     'Chicken soup is a soup made from chicken, simmered in water, usually with various other ingredients.
-                    The classic chicken soup consists of a clear chicken broth, often with pieces of chicken or vegetables; common additions are pasta, noodles, dumplings, or grains such as rice and barley.
-                    Chicken soup has acquired the reputation of a folk remedy for colds and influenza, and in many countries is considered a comfort food.',
+The classic chicken soup consists of a clear chicken broth, often with pieces of chicken or vegetables; common additions are pasta, noodles, dumplings, or grains such as rice and barley.
+Chicken soup has acquired the reputation of a folk remedy for colds and influenza, and in many countries is considered a comfort food.',
                     'Pat chicken dry with paper towels; sprinkle with salt and pepper.
-                    In a 6-qt. stockpot, heat oil over medium-high heat.
-                    Add chicken in batches, cook until dark golden brown, 3-4 minutes.
-                    Remove chicken from pan; discard all but 2 tablespoons drippings.
-                    Add onion to drippings; cook and stir over medium-high heat until tender, 4-5 minutes.
-                    Add garlic; cook 1 minute longer.
-                    Add broth, stirring to loosen browned bits from pan. Bring to a boil.
-                    Return chicken to pan.
-                    Add celery, carrots, bay leaves and thyme.
-                    Reduce heat; simmer, covered, until chicken is tender, 25-30 minutes.
-                    Transfer chicken to a plate. Remove soup from heat.
-                    Add noodles; let stand, covered, until noodles are tender, 20-22 minutes.
-                    Meanwhile, when chicken is cool enough to handle, remove meat from bones; discard bones. Shred meat into bite-sized pieces.
-                    Return meat to stockpot. Stir in parsley and lemon juice.
-                    If desired, adjust seasoning with additional salt and pepper. Discard bay leaves.',
+In a 6-qt. stockpot, heat oil over medium-high heat.
+Add chicken in batches, cook until dark golden brown, 3-4 minutes.
+Remove chicken from pan; discard all but 2 tablespoons drippings.
+Add onion to drippings; cook and stir over medium-high heat until tender, 4-5 minutes.
+Add garlic; cook 1 minute longer.
+Add broth, stirring to loosen browned bits from pan. Bring to a boil.
+Return chicken to pan.
+Add celery, carrots, bay leaves and thyme.
+Reduce heat; simmer, covered, until chicken is tender, 25-30 minutes.
+Transfer chicken to a plate. Remove soup from heat.
+Add noodles; let stand, covered, until noodles are tender, 20-22 minutes.
+Meanwhile, when chicken is cool enough to handle, remove meat from bones; discard bones. Shred meat into bite-sized pieces.
+Return meat to stockpot. Stir in parsley and lemon juice.
+If desired, adjust seasoning with additional salt and pepper. Discard bay leaves.',
                     'https://www.tasteofhome.com/recipes/the-ultimate-chicken-noodle-soup/'
                     );  
 
@@ -164,11 +164,11 @@ def insert_data():
                     VALUES(
                     'Banana Bread',
                     'Banana bread is a type of sweet bread made from mashed bananas.
-                    It is often a moist and sweet quick bread but some recipes are yeast raised.',
+It is often a moist and sweet quick bread but some recipes are yeast raised.',
                     'Preheat oven to 350°.
-                    In a large bowl, stir together flour, sugar, baking soda and salt.
-                    In another bowl, combine the eggs, bananas, oil, buttermilk and vanilla; add to flour mixture, stirring just until combined. If desired, fold in nuts.
-                    Pour into a greased or parchment-lined 9x5-in. loaf pan. Bake until a toothpick comes out clean, 1-1/4 to 1-1/2 hours. Cool in pan for 15 minutes before removing to a wire rack.',
+In a large bowl, stir together flour, sugar, baking soda and salt.
+In another bowl, combine the eggs, bananas, oil, buttermilk and vanilla; add to flour mixture, stirring just until combined. If desired, fold in nuts.
+Pour into a greased or parchment-lined 9x5-in. loaf pan. Bake until a toothpick comes out clean, 1-1/4 to 1-1/2 hours. Cool in pan for 15 minutes before removing to a wire rack.',
                     'https://www.tasteofhome.com/recipes/best-ever-banana-bread/'
                     );  
 
@@ -176,20 +176,20 @@ def insert_data():
                     VALUES(
                     'Homemade Bread',
                     'Bread is a staple food prepared from a dough of flour (usually wheat) and water, usually by baking.
-                    Throughout recorded history and around the world, it has been an important part of many cultures'' diet.
-                    It is one of the oldest human-made foods, having been of significance since the dawn of agriculture, and plays an essential role in both religious rituals and secular culture.',
+Throughout recorded history and around the world, it has been an important part of many cultures'' diet.
+It is one of the oldest human-made foods, having been of significance since the dawn of agriculture, and plays an essential role in both religious rituals and secular culture.',
                     'In a large bowl, dissolve yeast in warm water.
-                    Using a rubber spatula, stir in 3-1/2 cups flour and salt to form a soft, sticky dough. Do not knead.
-                    Cover and let rise at room temperature 1 hour.
-                    Stir down dough (dough will be sticky). Turn onto a floured surface; with floured hands pat into a 9-in. square.
-                    Fold square into thirds, forming a 9x3-in. rectangle. Fold rectangle into thirds, forming a 3-in. square.
-                    Place in a large greased bowl, turning once to grease the top. Cover and let rise at room temperature until almost doubled, about 1 hour.
-                    Punch down dough and repeat folding process. Return dough to bowl; refrigerate, covered, overnight.
-                    Grease the bottom of a disposable foil roasting pan with sides at least 4 in. high; dust pan with cornmeal.
-                    Turn dough onto a floured surface. Knead gently 6-8 times; shape into a 6-in. round loaf. Place into prepared pan; dust top with remaining 1 tablespoon flour.
-                    Cover pan and let rise at room temperature until dough expands to a 7-1/2-in. loaf, about 1-1/4 hours.
-                    Preheat oven to 500°. Using a sharp knife, make a slash (1/4 in. deep) across top of loaf. Cover pan tightly with foil. Bake on lowest oven rack 25 minutes.
-                    Reduce oven setting to 450°. Remove foil; bake bread until deep golden brown, 25-30 minutes. Remove loaf to a wire rack to cool.',
+Using a rubber spatula, stir in 3-1/2 cups flour and salt to form a soft, sticky dough. Do not knead.
+Cover and let rise at room temperature 1 hour.
+Stir down dough (dough will be sticky). Turn onto a floured surface; with floured hands pat into a 9-in. square.
+Fold square into thirds, forming a 9x3-in. rectangle. Fold rectangle into thirds, forming a 3-in. square.
+Place in a large greased bowl, turning once to grease the top. Cover and let rise at room temperature until almost doubled, about 1 hour.
+Punch down dough and repeat folding process. Return dough to bowl; refrigerate, covered, overnight.
+Grease the bottom of a disposable foil roasting pan with sides at least 4 in. high; dust pan with cornmeal.
+Turn dough onto a floured surface. Knead gently 6-8 times; shape into a 6-in. round loaf. Place into prepared pan; dust top with remaining 1 tablespoon flour.
+Cover pan and let rise at room temperature until dough expands to a 7-1/2-in. loaf, about 1-1/4 hours.
+Preheat oven to 500°. Using a sharp knife, make a slash (1/4 in. deep) across top of loaf. Cover pan tightly with foil. Bake on lowest oven rack 25 minutes.
+Reduce oven setting to 450°. Remove foil; bake bread until deep golden brown, 25-30 minutes. Remove loaf to a wire rack to cool.',
                     'https://www.tasteofhome.com/recipes/crusty-homemade-bread/'
                     );             
                     """)
